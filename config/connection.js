8c9da5e6cbccc07d9f2e8fdb0bf56a3412dcd29c
emmapankey <emmapankey19@gmail.com>
2018-02-01 12:16:10 -0500

    troubleshooting mongoose connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Require Mongoose
+var mongoose = require("Mongoose");
+
+// Database configuration with Mongoose
+mongoose.connect("mongodb://localhost/news-scraper");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once open log a successful connection
+db.once("open", function() {
+    console.log("Mongoose connection successful");
+});
+
+// Export the database
+module.exports = db;
