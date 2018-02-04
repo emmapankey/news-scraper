79cd02592615f866da05db6eaf63fef690c8d220
emmapankey <emmapankey19@gmail.com>
2018-02-04 14:55:13 -0500

    rendering articles to homepage

--- routes/routes.js
+++ routes/routes.js
@@ -14,19 +14,19 @@ var path = require("path");
 // 1. At the "/" path display every entry in the scraped collection 
 router.get("/", function (req, res) {
     // res.sendFile(path.join(__dirname, "../public/test.html"));
-    
+
     // Query: In the news database, go to the scraped collection, then "find" everything (all scraped articles)
     Scraped.find({}, function (error, found) {
         // Log any errors if the server encounters one
         if (error) {
             console.log(error);
         }
-        // Otherwise, send the result of this query to the browser as JSON
-        // PUT HANDLEBARS HERE
+        // RENDER HANDLEBARS HERE
         else {
+            // Otherwise, render the result of the query as handlebars
+            res.render("home", { article: found });
             // res.json(found);
-            // res.render("home");
-            res.sendFile(path.join(__dirname, "../public/test.html"));            
+            // res.sendFile(path.join(__dirname, "../public/test.html"));            
         }
     });
 });
@@ -34,12 +34,11 @@ router.get("/", function (req, res) {
 // 2. At the "/api/scrape" path call the scraper function.
 router.get("/api/scrape", function (req, res) {
     // Run the scrapeThis function from controller
-    scraper.scrapeThis(function() 
-	{
+    scraper.scrapeThis(function () {
         // After scraping redirect to home route to render all scraped articles
-		res.redirect("/");
-	});
-}); 
+        res.redirect("/");
+    });
+});
 
 // 3. At the "/saved" path display every entry in the saved collection
 router.get("/saved", function (req, res) {
