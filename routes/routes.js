ecc754873fb5410cfc837d1c4567f8344dbb62ec
emmapankey <emmapankey19@gmail.com>
2018-02-09 15:26:43 -0500

    working on delete route

--- routes/routes.js
+++ routes/routes.js
@@ -38,26 +38,17 @@ router.get("/api/scrape", function (req, res) {
 });
 
 // 3. At the "/api/save:id" path update the article's saved key to true in the Scraped document
-// router.get("/api/saved/:id", function (req, res) {
-//     console.log(req.params.id)
-//     db.Scraped.findOneAndUpdate({_id: req.params.id}, { $set: { saved: true} }, function(error, updated){
-//         if (error){
-//             console.log("Something wrong when updating data!");
-//         }
-//         else {
-//             console.log(updated);
-//         }
-//     });
-// });
-
 router.put("/api/saved/:id", function (req, res) {
+    
+    // Use the article id to find and update it's saved property
     var articleId = req.params.id;
     var updatedObj = {"saved": true};
         
-    Scraped.findByIdAndUpdate(articleId, updatedObj, {new: true}, function(err, model) {
+    Scraped.findByIdAndUpdate(articleId, updatedObj, {new: true}, function(err, found) {
         if (err) {
             console.log(err);
         }
+        res.render("saved", {saved: found})
     })
 });
 
@@ -74,4 +65,25 @@ router.get("/saved", function (req, res) {
     });
 });
 
+//6. Delete a saved article
+router.post("/api/delete/:id", function (req, res) {
+
+    // Use the article id to find and update it's saved property
+    // var articleId = req.params.id;    
+    Article.findOneAndUpdate({
+        "_id": req.params.id
+    }, { "saved": false })
+        // Execute the above query
+        .exec(function (err, doc) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            } else {
+                // Or send the document to the browser
+                console.log("deleted the article");
+                res.send(doc);
+            }
+        });
+}
+);
 module.exports = router;
