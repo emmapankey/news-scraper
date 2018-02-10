19554255dca138947330c53a5223b4e030a225dd
emmapankey <emmapankey19@gmail.com>
2018-02-09 22:11:00 -0500

    modals are disrupting routing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-
-    // Save an article function
-    // $("#saveBtn").on("click", function(event) {
-    $(document).on("click", "#saveBtn", function () {
-
-        var id = $(this).data("id");
-   
-        // Send the post request.
-        $.ajax("/api/saved/" + id, {
-            type: "PUT",
-        }).then(
-            function (result) {
-            }
-        );
+    
+        // Save an article function
+        // $("#saveBtn").on("click", function(event) {
+        $(document).on("click", "#saveBtn", function () {
+    
+            var id = $(this).data("id");
+       
+            // Send the post request.
+            $.ajax("/api/saved/" + id, {
+                type: "PUT",
+            }).then(
+                function (result) {
+                }
+            );
+        });
+    
+        // Delete a saved article function
+        $('#deleteBtn').click(function (event) {
+            var id = $(this).data("id");
+    
+            $.ajax("/api/delete/" + id, {
+                type:"PUT"
+            }).then(
+                function (result) {
+                }
+            );
+        });
+            
     });
-
-    // Delete a saved article function
-    $('#deleteBtn').click(function (event) {
-        var id = $(this).data("id");
-
-        $.ajax("/api/delete/" + id, {
-            type:"PUT"
-        }).then(
-            function (result) {
-            }
-        );
-    });
-        
-});
