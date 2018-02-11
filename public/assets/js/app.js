feab15456640c9a753c09a982ae53f84eb8ea561
emmapankey <emmapankey19@gmail.com>
2018-02-10 21:02:37 -0500

    modals in view with handlebars

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,64 @@
 $(document).ready(function () {
-    
-        // Save an article function
-        // $("#saveBtn").on("click", function(event) {
-        $(document).on("click", "#saveBtn", function () {
-    
-            var id = $(this).data("id");
-       
-            // Send the post request.
-            $.ajax("/api/saved/" + id, {
-                type: "PUT",
-            }).then(
-                function (result) {
-                }
-            );
-        });
-    
-        // Delete a saved article function
-        $('#deleteBtn').click(function (event) {
-            var id = $(this).data("id");
-    
-            $.ajax("/api/delete/" + id, {
-                type:"PUT"
-            }).then(
-                function (result) {
-                }
-            );
-        });
-            
+
+    $(document).on("click", "#scrape", function () {
+        // Scrape articles request
+        $.ajax({
+            method: "GET",
+            url: "/api/scrape"
+        }).done(function (data) {
+
+            // if (data == "") {
+            //     $("#noArticlesModal").modal();
+            // }
+            // else {
+            //      location.reload();
+            //     $("#scrapedModal").modal();
+            // }
+        })
+        $("#scrapedModal").modal();
     });
+
+    $(document).on("click", "#noteBtn", function () {
+        $("#noteModal").modal();
+    });
+
+    $(document).on("click", "#saveNote", function () {
+
+        var noteobj;
+
+        // Create a note request
+        $ajax({
+            method: "POST",
+            url: "/api/note",
+            data: noteObj
+        }).done(function (data) {
+
+        })
+    });
+
+    $(document).on("click", "#saveBtn", function () {
+        var id = $(this).data("id");
+
+        // Save an article request
+        $.ajax("/api/saved/" + id, {
+            type: "PUT",
+        }).done(function (result) {
+        });
+    });
+
+
+    $('#deleteBtn').click(function (event) {
+        var id = $(this).data("id");
+
+        // "Delete" a saved article request
+        $.ajax("/api/delete/" + id, {
+            type: "POST",
+            success: function(data){
+                if(data.success == true){ // if true
+                    location.reload(); // then reload the page
+                }
+            }
+        });
+    });
+
+});
